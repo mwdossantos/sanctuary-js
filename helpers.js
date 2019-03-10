b41fcec772d6152ce96f0efb6d24fced254eab6d
Matthew Wilson <matthew.martijn.wilson@gmail.com>
2019-03-10 20:52:49 +0100

    Logic

---
+++ helpers.js
@@ -0,0 +1,6 @@
+module.exports = {
+    Shuffle: function(o) {
+        for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    }
+}
--- m.js
+++ m.js
@@ -1,4 +1,7 @@
-const herbivore = [
+const random = require('random');
+const helpers = require('./helpers');
+
+const dinosaurs = [
     "Stygimoloch",
     "Sinoceratops",
     "Ankylosaurus",
@@ -7,10 +10,7 @@ const herbivore = [
     "Parasaurolophus",
     "Gallimimus",
     "Triceratops",
-    "Stegosaurus"
-];
-
-const carnivore = [
+    "Stegosaurus",
     "Indoraptor",
     "Carnotaurus",
     "Mosasaurus",
@@ -21,43 +21,140 @@ const carnivore = [
     "Compsognathus"
 ];
 
+const worldBounds = {
+    x: {
+        min: 0,
+        max: 100,
+    },
+    y: {
+        min: 0,
+        max: 100,
+    }
+}
 var groups = [];
 
 // Class declaration for a Dinosaur
 class Dinosaur {
 
-    constructor (diet) {
-        this.diet = diet;
-        this.name = this.pickRandomDinosaur();
+    constructor(id) {
+        this.name = `${id.toString()} ${this.pickRandomDinosaur()}`;
+        this.id = id;
         this.stats = this.generateStats();
         this.health = 100;
         this.thirst = 100;
         this.sleep = 100;
+        this.x = random.int(0, 100);
+        this.y = random.int(0, 100);
+
+        this.init();
+    }
+
+    init() {
+        setInterval(() => {
+            this.move();
+        }, 50);
     }
 
     // pick a random dinosaur with the given diet
-    pickRandomDinosaur () {
-        if (this.diet == 'Herbivore') {
-            return herbivore[Math.floor(Math.random() * herbivore.length)];
-        } else {
-            return carnivore[Math.floor(Math.random() * carnivore.length)];
-        }
+    pickRandomDinosaur() {
+        return dinosaurs[Math.floor(Math.random() * dinosaurs.length)];
     }
 
     // generate the attack and defense stats for the dinosaur
-    generateStats () {
-        
+    generateStats() {
+
         return {
-            attack: Math.floor(Math.random() * 100) + 50,
-            defense: Math.floor(Math.random() * 100) + 60,
+            attack: Math.floor(Math.random() * (100 - 50 + 1)) + 50,
+            defense: Math.floor(Math.random() * (100 - 70 + 1)) + 70,
         }
+    }
+
+    async move() {
+
+        var directions = {
+            right: true,
+            left: true,
+            up: true,
+            down: true,
+        }
+
+        if (this.x == worldBounds.x.max) {
+            directions.right = false;
+        }
+
+        if (this.x == worldBounds.x.min) {
+            directions.left = false;
+        }
+
+        if (this.y == worldBounds.y.max) {
+            directions.up = false;
+        }
+
+        if (this.y == worldBounds.y.min) {
+            directions.down = false;
+        }
+        // this.getDirection(directions)
+        const dir = this.getDirection(directions);
+
+        switch (dir) {
+            case 'up':
+                this.y++;
+                break;
+            case 'down':
+                this.y--;
+                break;
+            case 'right':
+                this.x++;
+                break;
+            case 'left':
+                this.x--;
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    getDirection(directions) {
+        var tempArray = [
+            {
+                direction: 'right',
+                status: directions.right
+            },
+            {
+                direction: 'left',
+                status: directions.left
+            },
+            {
+                direction: 'up',
+                status: directions.up
+            },
+            {
+                direction: 'down',
+                status: directions.down
+            },
+        ];
+
+        tempArray = helpers.Shuffle(tempArray);
+
+        for (let i = 0; i < tempArray.length; i++) {
+
+            if (tempArray[i].status == true && random.boolean()) {
+                return tempArray[i].direction;
+            }
+        }
+    }
+
+    kill () {
+        
     }
 }
 
 // Create a group of 5 from dinosaurs created with the Dinosaur class
 class Group {
 
-    constructor () {
+    constructor(name) {
+        this.name = name;
         this.dinosaurs = this.generateGroup();
     }
 
@@ -66,24 +163,17 @@ class Group {
 
         var group = [];
 
-        for (let i = 0; group.length < 5; i++) {
+        for (let i = 0; group.length < 100; i++) {
 
-            var groupMember = new Dinosaur('Herbivore');
-            
-            if (!checkIfExists(group, groupMember)) {
-                group.push(groupMember);
-            }
-    
+            var groupMember = new Dinosaur(group.length + 1);
+            group.push(groupMember);
+
         }
-        
+
         return group;
 
     }
 
-    // list the newly made group of dinosaurs
-    listGroup () {
-        return this.dinosaurs;
-    }
 }
 
 /** this function checks if the item to be added to an array already exists
@@ -94,12 +184,63 @@ function checkIfExists(group, dinosaur) {
 
     for (let i = 0; i < group.length; i++) {
         if (group[i].name == dinosaur.name) {
+            console.log('exists')
             return true;
         }
     }
     return false;
 }
 
-const group = new Group();
+var updateInterval;
 
-console.log(group.listGroup());
+function terminateProcess() {
+
+    console.warn("Stopping process...");
+    setTimeout(() => {
+        clearInterval(updateInterval);
+    }, 2000);
+}
+
+
+function update() {
+
+    updateInterval = setInterval(() => {
+    }, 1);
+}
+
+
+function start() {
+
+    checkIfCollide();
+    groups.push(new Group("dinos"));
+    update();
+
+}
+
+function checkIfCollide () {
+    
+    setInterval(() => {
+        var group = returnGroup('dinos');
+
+        for (let i = 0; i < group.dinosaurs.length; i++) {
+            var dino = group.dinosaurs[i];
+            for (let z = 0; z < group.dinosaurs.length; z++) {
+                if (dino.x == group.dinosaurs[z].x && dino.y == group.dinosaurs[z].y) {
+                    if (dino.name != group.dinosaurs[z].name) {
+                        console.log('same spot', dino.name,group.dinosaurs[z].name )
+                    }
+                }
+            }
+        }
+    }, 50);
+
+}
+
+function returnGroup (groupName) {
+    for (let i = 0; i < groups.length; i++) {
+        if (groups[i].name == groupName) {
+            return groups[i];
+        }
+    }
+}
+start();
